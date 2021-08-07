e63b6144d697b4957e48e97e2e03de8515b12170
maychaza <mayalejandrr@gmail.com>
2021-08-07 00:47:52 -0300

    Creo repo, service y controller de Aeropuerto, un model GenericResponse

--- src/main/java/ar/com/ada/api/aladas/entities/Aeropuerto.java
+++ src/main/java/ar/com/ada/api/aladas/entities/Aeropuerto.java
@@ -1,13 +1,7 @@
 package ar.com.ada.api.aladas.entities;
 
 import javax.annotation.Generated;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
+import javax.persistence.*;
 @Entity
 @Table(name= "aeropuerto")
 public class Aeropuerto {
---
+++ src/main/java/ar/com/ada/api/aladas/models/response/GenericResponse.java
@@ -0,0 +1,8 @@
+package ar.com.ada.api.aladas.models.response;
+
+public class GenericResponse {
+    
+    public boolean isOk;
+    public Integer id;
+    public String message;
+}
